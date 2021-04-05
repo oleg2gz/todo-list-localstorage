44cc26efa124f04190c08a7a8265c770217e9f64
Oleg Kravchenko <kitaist@kitaist.com>
2021-04-05 18:01:18 +0800

    feature/save todos to local storage

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const form = document.getElementById('new-todo-form')
 const input = document.getElementById('todo-input')
 const list = document.getElementById('list')
 const template = document.getElementById('list-item-template')
+const LOCAL_STORAGE_PREFIX = 'ADVANCED_TODO_LIST'
+const TODOS_LOCAL_STORAGE = `${LOCAL_STORAGE_PREFIX}-todos`
+const todos = []
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
 
   const todoContent = input.value
   if (todoContent === '') return
+  todos.push(todoContent)
+  saveTodos()
   renderTodos(todoContent)
   input.value = ''
 })
@@ -17,4 +22,8 @@ function renderTodos(todo) {
   const textElement = templateClone.querySelector('[data-list-item-text]')
   textElement.innerText = todo
   list.appendChild(templateClone)
+}
+
+function saveTodos() {
+  localStorage.setItem(TODOS_LOCAL_STORAGE, JSON.stringify(todos))
 }

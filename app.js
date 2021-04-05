51ce98194be36ab73d84335776b51c4ccf47e7a0
Oleg Kravchenko <kitaist@kitaist.com>
2021-04-05 17:45:03 +0800

    feature/add todos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const form = document.getElementById('new-todo-form')
+const input = document.getElementById('todo-input')
+const list = document.getElementById('list')
+const template = document.getElementById('list-item-template')
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const todoContent = input.value
+  if (todoContent === '') return
+  renderTodos(todoContent)
+  input.value = ''
+})
+
+function renderTodos(todo) {
+  const templateClone = template.content.cloneNode(true)
+  const textElement = templateClone.querySelector('[data-list-item-text]')
+  textElement.innerText = todo
+  list.appendChild(templateClone)
+}
